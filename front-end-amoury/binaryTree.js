cb486d4f812b899557630c70c8d2ffa6bd862ef1
代旭杰 <daixujie@daixujiedeMacBook-Pro.local>
2021-08-17 10:33:55 +0800

    feat(binaryTree.js): 锯齿形遍历

--- front-end-amoury/binaryTree.js
+++ front-end-amoury/binaryTree.js
@@ -6,7 +6,7 @@
 function postorderTraversal(root) {
   // 后续遍历
   if (!root) {
-    return root;
+    return [];
   }
   const stack = [root];
   const result = [];
@@ -31,13 +31,13 @@ function postorderTraversal(root) {
 }
 
 /**
- * 层级遍历
+ * 层级遍历，每一层依次访问
  * @param {TreeNode} root
  * @returns Array
  */
 function levelTraversal(root) {
   if (!root) {
-    return root;
+    return [];
   }
   const queue = [root];
   const result = [];
@@ -49,6 +49,42 @@ function levelTraversal(root) {
     }
     if (cur.right) {
       queue.push(cur.right);
+    }
+  }
+  return result;
+}
+
+/**
+ * 锯齿形（之字形）遍历，第一层从左到右，第二层从右到左，第三层从左到右...
+ * @param {TreeNode} root
+ * @returns {number[][]}二维数组
+ */
+function zigzagTraversal(root) {
+  if (!root) {
+    return [];
+  }
+  const result = [];
+  const queue = [];
+  queue.push({ depth: 0, node: root });
+
+  while (queue.length) {
+    const { depth, node } = queue.shift();
+    if (result[depth]) {
+      if (depth % 2 === 0) {
+        // 左 => 右
+        result[depth].push(node.value);
+      } else {
+        // 右 => 左
+        result[depth].unshift(node.value);
+      }
+    } else {
+      result[depth] = [node.value];
+    }
+    if (node.left) {
+      queue.push({ depth: depth + 1, node: node.left });
+    }
+    if (node.right) {
+      queue.push({ depth: depth + 1, node: node.right });
     }
   }
   return result;
@@ -79,5 +115,6 @@ const testTree = {
 module.exports = {
   postorderTraversal,
   levelTraversal,
+  zigzagTraversal,
   testTree,
 };
--- index.js
+++ index.js
@@ -2,19 +2,22 @@ const { LRU_PROTO, LRU_CLS } = require('./front-end-amoury/leastRecentlyUsed');
 const {
   postorderTraversal,
   levelTraversal,
-  // testTree,
+  zigzagTraversal,
+  testTree,
 } = require('./front-end-amoury/binaryTree');
 
-// const lru = new LRU_CLS(3);
-// console.log(lru);
+const lru = new LRU_CLS(3);
+console.log(lru);
 
-// const resultPostorder = postorderTraversal(testTree);
-// const resultLevel = levelTraversal(testTree);
-// console.log(resultPostorder, resultLevel);
+const resultPostorder = postorderTraversal(testTree);
+const resultLevel = levelTraversal(testTree);
+const resultZigzag = zigzagTraversal(testTree);
+console.log(resultPostorder, resultLevel, resultZigzag);
 
 module.exports = {
   LRU_PROTO,
   LRU_CLS,
   postorderTraversal,
   levelTraversal,
+  zigzagTraversal,
 };
